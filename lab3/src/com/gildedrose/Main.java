537813d05f54c2a3e016cc7da7660a3c68df7af5
Oleksandra <tykhonova.oleksandra@lll.kpi.ua>
2024-11-09 23:09:24 +0200

    Доданi елементи для тестування методу

--- lab3/src/com/gildedrose/Main.java
+++ lab3/src/com/gildedrose/Main.java
@@ -1,17 +1,29 @@
 package com.gildedrose;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 10, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 30),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Regular_item_1", 5, 7),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+                new Item("Regular_item_2", 3, 6)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        System.out.println("Before inventorization:");
+        for (Item item : items) {
+            System.out.println(item);
+        }
+
+        app.updateQuality();
+
+        System.out.println("\nAfter inventorization:");
+        for (Item item : items) {
+            System.out.println(item);
         }
     }
 }
